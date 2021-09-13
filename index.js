293511346c62ac5bd9466652d0f093fc747bf58e
Lun-Dev <lunthedev@gmail.com>
2021-09-13 17:38:59 +0800

    Revised index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+let countEl = document.getElementById("count-el")
 let count = 0
 
 function increment() {
     count += 1
-    document.getElementById("count-el").innerText = count
+    countEl.innerText = count
 }
