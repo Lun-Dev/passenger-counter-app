57d7827172bdaf8b0ee04f495eefec9835a60046
Lun-Dev <lunthedev@gmail.com>
2021-09-14 15:00:47 +0800

    Counter reset

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ let count = 0
 
 function increment() {
     count += 1
-    countEl.innerText = count
+    countEl.textContent = count
 }
 
 function save() {
     let countStr = count + " - "
-    saveEl.innerText += countStr
+    saveEl.textContent += countStr
+    count = 0
+    countEl.textContent = 0
 }
